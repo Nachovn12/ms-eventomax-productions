e506d24f30abc4bb17cf32a33fcfd805fdb85510
BenjaFlores379 <benja.floresa@duocuc.cl>
2026-09-15 23:23:57 -0300

    Handle missing production response

--- src/main/java/cl/duoc/eventomax/productions/service/ResourceNotFoundException.java
+++ src/main/java/cl/duoc/eventomax/productions/service/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package cl.duoc.eventomax.productions.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
