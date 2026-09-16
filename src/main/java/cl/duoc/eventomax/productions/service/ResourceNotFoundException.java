17e17ca93e70282242126bb1ee748dc166f1923a
BenjaFlores379 <benja.floresa@duocuc.cl>
2026-09-15 23:12:53 -0300

    Add not found handling for production lookup

---
+++ src/main/java/cl/duoc/eventomax/productions/service/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package cl.duoc.eventomax.productions.service;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
