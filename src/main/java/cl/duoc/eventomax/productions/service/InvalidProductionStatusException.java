2e44c1831398ef922fcbe67145703325dbd94e63
Ignacio Valeria <ig.valeria@duocuc.cl>
2026-09-19 21:56:52 -0300

    feat(productions): add explicit invalid status exception

---
+++ src/main/java/cl/duoc/eventomax/productions/service/InvalidProductionStatusException.java
@@ -0,0 +1,8 @@
+package cl.duoc.eventomax.productions.service;
+
+public class InvalidProductionStatusException extends RuntimeException {
+
+    public InvalidProductionStatusException(String message) {
+        super(message);
+    }
+}
